b54497f14d420fb15d9758d3e2359da31860b2f2
vysakh96 <118036322+vysakh96@users.noreply.github.com>
2022-12-29 10:58:14 +0530

    updated

--- lib/result_page.dart
+++ lib/result_page.dart
@@ -2,40 +2,41 @@ import 'package:flutter/material.dart';
 
 class resultPage extends StatefulWidget {
   // const resultPage({Key? key}) : super(key: key);
-  double s;
-  int w;
-  int a;
-  resultPage(this.s,this.w,this.a);
+  double height;
+  int weight;
+  int age;
+  resultPage(this.height, this.weight, this.age);
 
   @override
   State<resultPage> createState() => _resultPageState();
 }
 
 class _resultPageState extends State<resultPage> {
-
-  void initState(){
+  void initState() {
     super.initState();
     calculation();
   }
-  double result = 0, h = 0;
+
+  double result = 0, heightInMeter = 0;
   String summary = '';
-  void calculation(){
+  void calculation() {
     setState(() {
-      h = widget.s/100;
-      result = widget.w/(h*h);
-      if(result<18.5){
+      heightInMeter = widget.height / 100;
+      result = widget.weight / (heightInMeter * heightInMeter);
+      if (result < 18.5) {
         summary = 'Under Weight';
-      }else if(result>=18.5 && result<=24.9){
+      } else if (result >= 18.5 && result <= 24.9) {
         summary = 'Normal';
-      }else if(result>=25 && result<=29.9){
+      } else if (result >= 25 && result <= 29.9) {
         summary = 'Over Weight';
-      }else if(result>=30 && result<=39.9) {
+      } else if (result >= 30 && result <= 39.9) {
         summary = 'Obese';
-      }else{
+      } else {
         summary = 'Extremely Obese';
       }
     });
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -46,16 +47,19 @@ class _resultPageState extends State<resultPage> {
         children: [
           Padding(
             padding: const EdgeInsets.all(10.0),
-            child: Text('RESULT',
-            style: TextStyle(
-                fontWeight:FontWeight.bold,fontSize: 30,color: Colors.red ),),
+            child: Text(
+              'RESULT',
+              style: TextStyle(
+                  fontWeight: FontWeight.bold, fontSize: 30, color: Colors.red),
+            ),
           ),
           Container(
             padding: EdgeInsets.all(40),
             margin: EdgeInsets.all(10),
             width: double.maxFinite,
             decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(15),color:Colors.grey[800],
+              borderRadius: BorderRadius.circular(15),
+              color: Colors.grey[800],
             ),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.center,
@@ -63,79 +67,131 @@ class _resultPageState extends State<resultPage> {
                 Column(
                   crossAxisAlignment: CrossAxisAlignment.end,
                   children: [
-                    Text('HEIGHT',
-                    style: TextStyle(
-                      fontSize: 20,color: Colors.white70,
-                    ),),
-                    SizedBox(height: 5,),
-                    Text('WEIGHT',
+                    Text(
+                      'HEIGHT',
                       style: TextStyle(
-                        fontSize: 20,color: Colors.white70,
-                      ),),
-                    SizedBox(height: 5,),
-                    Text('AGE',
+                        fontSize: 20,
+                        color: Colors.white70,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    Text(
+                      'WEIGHT',
                       style: TextStyle(
-                        fontSize: 20,color: Colors.white70,
-                      ),),
-                    SizedBox(height: 10,),
-                    Text('RESULT',
+                        fontSize: 20,
+                        color: Colors.white70,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    Text(
+                      'AGE',
                       style: TextStyle(
-                        fontSize: 20,color: Colors.white70,
-                      ),),
+                        fontSize: 20,
+                        color: Colors.white70,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      'RESULT',
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.white70,
+                      ),
+                    ),
                   ],
                 ),
-                SizedBox(width: 20,),
+                SizedBox(
+                  width: 20,
+                ),
                 Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text('${widget.s.round().toString()} cm',
+                    Text(
+                      '${widget.height.round().toString()} cm',
                       style: TextStyle(
-                        fontSize: 20,color: Colors.white,fontWeight: FontWeight.bold,
-                      ),),
-                    SizedBox(height: 5,),
-                    Text('${widget.w} kg',
+                        fontSize: 20,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    Text(
+                      '${widget.weight} kg',
                       style: TextStyle(
-                        fontSize: 20,color: Colors.white,fontWeight: FontWeight.bold,
-                      ),),
-                    SizedBox(height: 5,),
-                    Text('${widget.a}',
+                        fontSize: 20,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    Text(
+                      '${widget.age}',
                       style: TextStyle(
-                        fontSize: 20,color: Colors.white,fontWeight: FontWeight.bold,
-                      ),),
-                    SizedBox(height: 5,),
-                    Text('${result.toStringAsFixed(2)}',
+                        fontSize: 20,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    Text(
+                      '${result.toStringAsFixed(2)}',
                       style: TextStyle(
-                        fontSize: 20,color: Colors.white,fontWeight: FontWeight.bold
-                      ),),
+                          fontSize: 20,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold),
+                    ),
                   ],
                 ),
               ],
             ),
           ),
-            Center(
-              child: SingleChildScrollView(scrollDirection: Axis.horizontal,
-                child: Text('SUMMARY : $summary',
+          Center(
+            child: SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Text(
+                'SUMMARY : $summary',
                 style: TextStyle(
-                  fontSize: 25,color: Colors.yellow,fontWeight: FontWeight.w500,
-                ),
+                  fontSize: 25,
+                  color: Colors.yellow,
+                  fontWeight: FontWeight.w500,
                 ),
               ),
             ),
-          SizedBox(height: 30,),
+          ),
+          SizedBox(
+            height: 30,
+          ),
           Container(
             height: 50,
             width: double.maxFinite,
             color: Colors.red,
-            child: TextButton(onPressed: (){
-              Navigator.pop(context);
-            },
-                child: Text('CALCULATE AGAIN',
-                    style: TextStyle(
-                      fontSize: 20,color: Colors.white,fontWeight: FontWeight.bold,),)),
+            child: TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text(
+                  'CALCULATE AGAIN',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                )),
           ),
         ],
       ),
     );
   }
-
 }
